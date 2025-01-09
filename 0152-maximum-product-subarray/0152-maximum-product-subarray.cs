8ca1e0040151d2d13203ea915c3cf8170b85d7ff
Junho Shin <72008909+shinjuno123@users.noreply.github.com>
2025-01-08 22:18:01 -0800

    Time: 1 ms (63.33%), Space: 43 MB (73.35%) - LeetHub

---
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.cs
@@ -0,0 +1,26 @@
+public class Solution {
+    public int MaxProduct(int[] nums) {
+        // set 2 variables with the first index value
+        // Time Complexity O(N) and Auxilary space will be O(1)
+        int maxProduct = nums[0];
+
+        int globalMax = nums[0];
+        int globalMin = nums[0];
+
+        for (int i=1; i < nums.Length; ++i){
+            int localMax = globalMax * nums[i];
+            int localMin = globalMin * nums[i];
+
+            globalMax = Math.Max(localMin, localMax);
+            globalMax = Math.Max(nums[i], globalMax);
+
+            globalMin = Math.Min(localMin, localMax);
+            globalMin = Math.Min(nums[i], globalMin);
+
+            maxProduct = Math.Max(maxProduct, globalMax);
+        }
+
+        return maxProduct;
+
+    }
+}
