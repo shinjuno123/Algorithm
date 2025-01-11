f0aabd60852da9abe42360e8b0e2472e69703c93
Junho Shin <72008909+shinjuno123@users.noreply.github.com>
2025-01-11 15:12:42 -0800

    Time: 4 ms (2.78%), Space: 42.4 MB (5.56%) - LeetHub

---
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.cs
@@ -0,0 +1,9 @@
+public class Solution {
+    public int FindMin(int[] nums) {
+        // Time Complexity O(n) Space Complexity O(1)
+        // Iterate over the array and find the lowest value
+
+        return nums.Min();
+
+    }
+}
