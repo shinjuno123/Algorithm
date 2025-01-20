a925cd296f974f7d4f7d629281436273118643b5
Junho Shin <72008909+shinjuno123@users.noreply.github.com>
2025-01-19 22:39:05 -0800

    Time: 0 ms (100%), Space: 18.1 MB (24.42%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
@@ -1,39 +1,36 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        # 4,5,6,7,0,1,2,d,d,d,d
-        # target = 5
-        # 2,3,4,5,1
-        # target = 3
-        # Time complexity O(log N) Space O(1)
-        # mid = left + (right - left) / 2 
-        left: int = 0
-        right: int = len(nums) - 1
-        pivot = 0
+        left = 0
+        right = len(nums) - 1
 
         while left <= right:
             mid = (right - left) // 2 + left
-
-            if nums[right] < nums[mid]:
-                left = mid + 1
-            elif nums[right] > nums[mid]:
-                right = mid
+            
+            # If target is already at the mid, 
+            if nums[mid] == target:
+                # return it
+                return mid
+            
+            # If left half is sorted
+            elif nums[left] <= nums[mid]:
+                # Check if target is lying in the left side
+                if nums[left] <= target < nums[mid]:
+                    # move right pointer to left side
+                    right = mid - 1
+                # if target is lying in the right side
+                else:
+                    # move the left pointer to the right side
+                    left = mid + 1
+            # If right half is sorted
             else:
-                pivot = mid
-                break
+                # check if the target is lying in the right side
+                if nums[mid] < target <= nums[right]:
+                    # Move the left pointer to the right side
+                    left = mid + 1
+                # if target is lying in the left side
+                else:
+                    # Move the right pointer to the left side
+                    right = mid - 1
         
-        left = pivot
-        right = (pivot - 1) + len(nums)
-
-        while left <= right:
-            mid = (right - left) // 2 + left
-            actual_mid = mid % len(nums)
-
-            if nums[actual_mid] < target:
-                left = mid + 1
-            elif nums[actual_mid] > target:
-                right = mid - 1
-            else:
-                return actual_mid
-
         return -1
         
