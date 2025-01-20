3d8097048a27857ba30bd9a71d8444b7d4f1be50
Junho Shin <72008909+shinjuno123@users.noreply.github.com>
2025-01-19 22:19:03 -0800

    Time: 0 ms (100%), Space: 18.2 MB (13.03%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
@@ -1,11 +1,39 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        # 4,5,6,7,0,1,2
-        # Time complexity O(N) Space O(1)
-        # Iterate over the list and check where the target is
-        # When we found the target, we will return the index number right away
-        for i in range(0,len(nums)):
-            if nums[i] == target:
-                return i
+        # 4,5,6,7,0,1,2,d,d,d,d
+        # target = 5
+        # 2,3,4,5,1
+        # target = 3
+        # Time complexity O(log N) Space O(1)
+        # mid = left + (right - left) / 2 
+        left: int = 0
+        right: int = len(nums) - 1
+        pivot = 0
+
+        while left <= right:
+            mid = (right - left) // 2 + left
+
+            if nums[right] < nums[mid]:
+                left = mid + 1
+            elif nums[right] > nums[mid]:
+                right = mid
+            else:
+                pivot = mid
+                break
+        
+        left = pivot
+        right = (pivot - 1) + len(nums)
+
+        while left <= right:
+            mid = (right - left) // 2 + left
+            actual_mid = mid % len(nums)
+
+            if nums[actual_mid] < target:
+                left = mid + 1
+            elif nums[actual_mid] > target:
+                right = mid - 1
+            else:
+                return actual_mid
 
         return -1
+        
