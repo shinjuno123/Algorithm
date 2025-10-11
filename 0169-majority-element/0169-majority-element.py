6b097e065ac4a7e9d41cb59d2a05b1b7e7019166
Junho Shin <72008909+shinjuno123@users.noreply.github.com>
2025-10-11 08:06:58 -0700

    Time: 15 ms (14.57%), Space: 19.3 MB (86.96%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,16 +1,15 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
+        # O (N) O (N)
         dic = {}
-
-        for num in nums:
-            dic[num] = 1 + dic.get(num, 0)
-        
         max_val = float('-inf')
         max_key = None
 
-        for key in dic:
-            if max_val < dic[key]:
-                max_val = dic[key]
-                max_key = key
+        for num in nums:
+            dic[num] = 1 + dic.get(num, 0)
+
+            if max_val < dic[num]:
+                max_val = dic[num]
+                max_key = num
 
         return max_key
