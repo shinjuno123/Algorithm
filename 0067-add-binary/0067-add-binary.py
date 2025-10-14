f32316e2b763aae4c3614931416c0e104ddeee32
Junho Shin <72008909+shinjuno123@users.noreply.github.com>
2025-10-13 18:49:47 -0700

    Time: 4 ms (17.49%), Space: 17.8 MB (52.57%) - LeetHub

---
+++ 0067-add-binary/0067-add-binary.py
@@ -0,0 +1,29 @@
+class Solution:
+    def addBinary(self, a: str, b: str) -> str:
+        over = 0
+        result = []
+        if len(a) < len(b):
+            return self.addBinary(b, a)
+        
+        b = "0" * (len(a) - len(b)) + b
+        print(a,  b)
+        
+        # a is always longer than or equal to the length of b
+
+        for i in range(len(a) - 1, -1, -1):
+            a_digit = int(a[i])
+            b_digit = int(b[i]) if len(b) > i else 0
+            current_digit_sum = a_digit + b_digit + over
+            if current_digit_sum >= 2:
+                over = 1
+                result.append(str(current_digit_sum - 2))
+            else:
+                over = 0
+                result.append(str(current_digit_sum))
+        
+        if over > 0:
+            result.append(str(over))
+            over = 0
+        
+        return ''.join(reversed(result))
+            
