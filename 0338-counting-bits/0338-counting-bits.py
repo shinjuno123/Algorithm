3527d1e23b78bb2ae310a79b245369ab63934e45
Junho Shin <72008909+shinjuno123@users.noreply.github.com>
2025-10-27 18:38:08 -0700

    Time: 3 ms (93.94%), Space: 18.5 MB (59.67%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.py
+++ 0338-counting-bits/0338-counting-bits.py
@@ -1,12 +1,12 @@
 class Solution:
     def countBits(self, n: int) -> List[int]:
-        ans = []
-        for i in range(0, n + 1):
-            b = bin(i)[2:]
-            total = 0
-            for number_string in b:
-                total += int(number_string)
+        dp = [0] * (n + 1)
+        sub = 1
+
+        for i in range(1, n + 1):
+            if sub * 2 == i:
+                sub = i
             
-            ans.append(total)
-        return ans
-            
+            dp[i] = dp[i - sub] + 1
+
+        return dp
