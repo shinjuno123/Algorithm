8c3e5f52754a16655d9a663fb91cd6ea31ffac97
Junho Shin <72008909+shinjuno123@users.noreply.github.com>
2025-12-20 13:10:47 -0800

    Time: 0 ms (100%), Space: 17.3 MB (99.39%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
@@ -12,15 +12,19 @@ class Solution:
             if list1.val <= list2.val:
                 cur.next = list1
                 list1 = list1.next
+            
             elif list1.val > list2.val:
                 cur.next = list2
                 list2 = list2.next
+            
             cur = cur.next
+
+            
         
         if list1:
             cur.next = list1
         else:
             cur.next = list2
-        
+
         return dummy.next
         
