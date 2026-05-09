8cf2b11b1dd1e398a0a41e53c6c3acfac67dc1e5
Junho Shin <72008909+shinjuno123@users.noreply.github.com>
2026-05-09 15:03:17 -0700

    Time: 1958 ms (20.88%), Space: 27.8 MB (5.02%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -3,21 +3,16 @@ class Solution:
         if len(s) <= 1:
             return s
         
-        def expand_from_center(left, right):
-            while left >= 0 and right < len(s) and s[left] == s[right]:
-                left -= 1
-                right += 1
-            
-            return s[left + 1: right]
-        
+        max_len = 1
         max_str = s[0]
-        for i in range(len(s) - 1):
-            odd = expand_from_center(i, i)
-            even = expand_from_center(i, i + 1)
-            
-            if len(odd) > len(max_str):
-                max_str = odd
-            if len(even) > len(max_str):
-                max_str = even
+        dp = [[False for _ in range(len(s))] for _ in range(len(s))]
+        for i in range(len(s)):
+            dp[i][i] = True
+            for j in range(i):
+                if s[j] == s[i] and (i - j <= 2 or dp[j + 1][i - 1]):
+                    dp[j][i] = True
+                    if i - j + 1 > max_len:
+                        max_len = i - j + 1
+                        max_str = s[j: i + 1]
 
-        return max_str
+        return max_str 
